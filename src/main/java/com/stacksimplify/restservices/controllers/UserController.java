c3d57265e3396745b4324c8b384c03256ea73d21
Shubham Gupta <505mytechwork@gmail.com>
2020-11-21 15:50:04 +0530

    JPA Base Restservice Development Completed

---
+++ src/main/java/com/stacksimplify/restservices/controllers/UserController.java
@@ -0,0 +1,102 @@
+package com.stacksimplify.restservices.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.stacksimplify.restservices.entities.User;
+import com.stacksimplify.restservices.services.UserService;
+
+//Controller
+@RestController
+public class UserController {
+	//Autowire the User Servise
+	
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping("/users")
+	public List<User> getAllUsers(){
+		return userService.getAllUsers();
+	}
+	
+	//createUser
+	//@ReuqestBody Annotation
+	//@PostMapping Annotation
+	@PostMapping("/users")
+	public User createUser(@RequestBody User user) {
+		return userService.createUser(user);
+	}
+	
+	
+	//getUserById
+	//@GetMapping
+	//@PathVariable
+	@GetMapping("/user/{id}")
+	public Optional<User> getUserById(@PathVariable("id") Long id){
+		return userService.getUserById(id);
+	}
+	
+	
+	//updateUserById
+	//@Pathvariable, @RequestBody
+	//@PutMapping
+	@PutMapping("/update-user/{id}")
+	public User updateUserById(@PathVariable("id") Long id, @RequestBody User user) {
+		return userService.updateUserById(id, user);
+	}
+	
+	//deleteUserById
+	//@DeleteMapping
+	//@PathVariable
+	@DeleteMapping("/delete/{id}")
+	public void deleteUserById(@PathVariable("id") Long id) {
+		userService.deleteUserById(id);
+	} 
+	
+	//getUserName
+	//@GetMapping
+	//@PathVariable
+	@GetMapping("/users/byusername/{username}")
+	public User getUserByUserName(@PathVariable("username") String username) {
+		return userService.getUserByUsername(username);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values(101, 'Shubham@test.com', 'Shubham', 'Gupta', 'admin', 'ssn101', 'Shubham');
+insert into user values(102, 'Riya@test.com', 'Riya', 'Gupta', 'admin', 'ssn102', 'Riya');
+insert into user values(103, 'Rupali@test.com', 'Rupali', 'Gupta', 'admin', 'ssn103', 'Rupali');
