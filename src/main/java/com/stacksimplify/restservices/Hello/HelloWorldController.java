79f1be33e3d4435cb2d5b92fffc464072ec9227d
Shubham Gupta <505mytechwork@gmail.com>
2021-02-02 15:52:47 +0530

    Internationalization first commit

--- src/main/java/com/stacksimplify/restservices/Hello/HelloWorldController.java
+++ src/main/java/com/stacksimplify/restservices/Hello/HelloWorldController.java
@@ -1,11 +1,21 @@
 package com.stacksimplify.restservices.Hello;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 //Controller
 @RestController
 public class HelloWorldController {
+	
+	@Autowired
+	private ResourceBundleMessageSource messageSource;
+	
 	
 	//Simple Method
 	//URI - /helloworld
@@ -23,4 +33,16 @@ public class HelloWorldController {
 	}
 	
 	
+	@GetMapping("hello-int")
+	public String getMessagesInI18Format(@RequestHeader(name = "Accept-Language", required = false) String locale)
+	{
+		return messageSource.getMessage("label.hello", null,new Locale(locale));
+	}
+	
+	
+	@GetMapping("hello-int2")
+	public String getMessagesInI18Format2()
+	{
+		return messageSource.getMessage("label.hello", null,LocaleContextHolder.getLocale());
+	}
 }
